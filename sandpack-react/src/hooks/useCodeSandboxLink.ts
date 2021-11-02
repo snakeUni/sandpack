f7c4af7d1211099cddb9e410cd363c8eb83e4324
Danilo Woznica <danilowoz@gmail.com>
2021-11-02 14:07:00 +0000

    refactor(useCodeSandboxLink): debounce changes

--- sandpack-react/src/hooks/useCodeSandboxLink.ts
+++ sandpack-react/src/hooks/useCodeSandboxLink.ts
@@ -1,6 +1,6 @@
 import type { SandpackBundlerFiles } from "@codesandbox/sandpack-client";
 import { getParameters } from "codesandbox-import-utils/lib/api/define";
-import * as React from "react";
+import { useState, useEffect } from "react";
 
 import type { SandboxEnvironment } from "../types";
 
@@ -10,34 +10,53 @@ const getFileParameters = (
   files: SandpackBundlerFiles,
   environment?: SandboxEnvironment
 ) => {
-  const normalized: Record<
+  type NormalizedFiles = Record<
     string,
-    { content: string; isBinary: boolean }
-  > = Object.keys(files).reduce(
-    (prev, next) => ({
-      ...prev,
-      [next.replace("/", "")]: {
-        content: files[next].code,
-        isBinary: false,
-      },
-    }),
-    {}
-  );
+    {
+      content: string;
+      isBinary: boolean;
+    }
+  >;
+
+  const normalizedFiles = Object.keys(files).reduce((prev, next) => {
+    const fileName = next.replace("/", "");
+    const value = {
+      content: files[next].code,
+      isBinary: false,
+    };
+
+    return { ...prev, [fileName]: value };
+  }, {} as NormalizedFiles);
 
   return getParameters({
-    files: normalized,
+    files: normalizedFiles,
     ...(environment ? { template: environment } : null),
   });
 };
 
 export const useCodeSandboxLink = (): string => {
+  const [paramsValues, setParamsValues] = useState("");
   const { sandpack } = useSandpack();
-  const params = getFileParameters(sandpack.files, sandpack.environment);
+
+  useEffect(
+    function debounce() {
+      const timer = setTimeout(() => {
+        const params = getFileParameters(sandpack.files, sandpack.environment);
+
+        setParamsValues(params);
+      }, 600);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    },
+    [sandpack.environment, sandpack.files]
+  );
 
   // Register the usage of the codesandbox link
-  React.useEffect(() => {
+  useEffect(function registerUsage() {
     sandpack.openInCSBRegisteredRef.current = true;
   }, []);
 
-  return `https://codesandbox.io/api/v1/sandboxes/define?parameters=${params}&query=file=${sandpack.activePath}%26from-sandpack=true`;
+  return `https://codesandbox.io/api/v1/sandboxes/define?parameters=${paramsValues}&query=file=${sandpack.activePath}%26from-sandpack=true`;
 };
